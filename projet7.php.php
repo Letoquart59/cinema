93db5ece991f02ea9d0b62d723d708a5a45416ee
letoquart59 <letoquartmichael@gmail.com>
2023-06-27 16:00:57 +0200

    cinema

---
+++ projet7.php.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cinéma</title>
+</head>
+<body>
+    <?php
+ 
+        $tab=
+            ["Nom" => "10 jours encore sans maman",
+            "Date" => "12/04/2023",
+            "Réalisateur" => "Ludovic Bernard",
+            "Durée" => 96,
+            "Genre" => "Comédie",
+            "Synopsis" => "Après son licenciement, Antoine, ancien DRH d'une grande enseigne de bricolage, a choisi de rester à la maison pour s'occuper de ses 4 enfants. Un nouveau travail qu'il effectue la plupart du temps seul, car sa femme Isabelle est très occupée par sa nouvelle activité d'avocate. Depuis deux ans dans la famille Mercier, les rôles ont donc clairement été inversés et Antoine commence à de moins en moins tenir le coup face à l'énergie que lui demande sa petite famille. Voilà pourquoi 10 jours de vacances à la montagne s'annoncent comme une aubaine pour le père au foyer qu'il est devenu. Hélas, une affaire inespérée pour le cabinet d'Isabelle tombe du ciel. Elle n'a pas d'autres solutions que de laisser Antoine partir 10 jours au ski seul avec les 4 enfants, et surtout : sans maman !",
+            "Bane annonce" => '<iframe src= "https://www.youtube.com/embed/VRDglZvHa-8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+            "Image" => "C:\Users\DWWM\Desktop\Xampp\htdocs\Projet7\10_jours_encore_sans_maman_affiche.jpg"
+            ],
+            
+            ["Nom" => "65- La terre d'avant",
+            "Date" => "15/03/2023",
+            "Réalisateur" => "Scott Berck, Bryan Woods",
+            "Durée" => 93,
+            "Genre" => "Action, science-fiction, thriller",
+            "Synopsis" => "Après un terrible crash sur une planète inconnue, le pilote Mills découvre rapidement qu'il a en réalité échoué sur Terre… il y a 65 millions d'années. Pour réussir leur unique chance de sauvetage, Mills et Koa l'unique autre survivante du crash, doivent se frayer un chemin à travers des terres inconnues peuplées de dangereuses créatures préhistoriques dans un combat épique pour leur survie.",
+            "Bane annonce" => '<iframe width="560" height="315" src="https://www.youtube.com/embed/RF4pH6Xsorc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+            "Image" => "C:\Users\DWWM\Desktop\Xampp\htdocs\Projet7\10_jours_encore_sans_maman_affiche.jpg"
+            ],
+
+            ["Nom" => "65- La terre d'avant",
+            "Date" => "15/03/2023",
+            "Réalisateur" => "Scott Berck, Bryan Woods",
+            "Durée" => 93,
+            "Genre" => "Action, science-fiction, thriller",
+            "Synopsis" => "Après un terrible crash sur une planète inconnue, le pilote Mills découvre rapidement qu'il a en réalité échoué sur Terre… il y a 65 millions d'années. Pour réussir leur unique chance de sauvetage, Mills et Koa l'unique autre survivante du crash, doivent se frayer un chemin à travers des terres inconnues peuplées de dangereuses créatures préhistoriques dans un combat épique pour leur survie.",
+            "Bane annonce" => '<iframe width="560" height="315" src="https://www.youtube.com/embed/RF4pH6Xsorc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+            "Image" => "C:\Users\DWWM\Desktop\Xampp\htdocs\Projet7\10_jours_encore_sans_maman_affiche.jpg"
+            ],
+            ["Nom" =>"apaches",
+            
+
+            
+ 
+        ],
+    ?>    
+
+</body>
+</html>
